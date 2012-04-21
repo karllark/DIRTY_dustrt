5b823184bca7c72d14e1bc60c1bfc0395d76853b
dirty <dirty@0244ea3c-2337-42f8-bec0-669938899761>
2012-04-21 03:01:52 +0000

    Speed up the code by figuring out the scattering position from the saved path info instead of calling calc_photon_trajectory() a second time.

--- trunk/include/scatter_photon.h
+++ trunk/include/scatter_photon.h
@@ -13,11 +13,4 @@
 //**********************************************************************
 // external function definitions
 
-// determines the photon trajectory (returns the distance and tau traveled)
-extern double calc_photon_trajectory (photon_data& photon,
-                                      geometry_struct& geometry,
-                                      double target_tau,
-                                      int& escape,
-                                      double& tau_traveled);
-
 #endif
