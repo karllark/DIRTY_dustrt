88aafd4f543fa44c9f86924c88083bdd1d932850
Karl Gordon <kgordon@stsci.edu>
2018-05-31 18:21:28 -0400

    Adding in output of number H atoms per cell

--- output_model_grid.cpp
+++ output_model_grid.cpp
@@ -4,7 +4,7 @@
 // 2008 Aug/KDG - written
 // ======================================================================
 #include "output_model_grid.h"
-//#define DEBUG_OMG
+#define DEBUG_OMG
 
 void output_model_grid (geometry_struct& geometry,
 			output_struct& output,
@@ -25,14 +25,17 @@ void output_model_grid (geometry_struct& geometry,
   string filename_tau = "!" + output.file_base;
   filename_tau += "_tau_ref_per_pc.fits";
 
+	string filename_num_H = "!" + output.file_base;
+  filename_num_H += "_num_H.fits";
+
   string filename_pos = "!" + output.file_base;
   filename_pos += "_pos.fits";
-      
+
 #ifdef DEBUG_OMG
   cout << "filename for output (rad_field) = " << filename << endl;
   cout << "filename for output (tau_ref_per_pc) = " << filename_tau << endl;
 #endif
-      
+
   // create a FITS file with extensions to fill with the output of the model
   int status = 0;
   fitsfile *out_ptr;   // pointer to the output fits file
@@ -43,10 +46,13 @@ void output_model_grid (geometry_struct& geometry,
   fits_create_file(&out_unc_ptr,filename_unc.c_str(), &status);
   check_fits_io(status, "fits_create_file : output_model_grid (rad_field unc)");
 
-  // create a FITS file with extensions to fill with the output of the model
   fitsfile *out_tau_ptr;   // pointer to the output fits file
   fits_create_file(&out_tau_ptr,filename_tau.c_str(), &status);
   check_fits_io(status, "fits_create_file : output_model_grid (tau)");
+
+	fitsfile *out_num_H_ptr;   // pointer to the output fits file
+  fits_create_file(&out_num_H_ptr,filename_num_H.c_str(), &status);
+  check_fits_io(status, "fits_create_file : output_model_grid (num_H)");
 
   // create a FITS file with extensions to fill with the wavelength grid
   fitsfile *out_wave_ptr;   // pointer to the output fits file
@@ -57,7 +63,7 @@ void output_model_grid (geometry_struct& geometry,
   fitsfile *out_pos_ptr;   // pointer to the output fits file
   fits_create_file(&out_pos_ptr,filename_pos.c_str(), &status);
   check_fits_io(status, "fits_create_file : output_model_grid (pos)");
-      
+
 #ifdef DEBUG_OMG
   cout << "entering the big copy loop" << endl;
 #endif
@@ -78,9 +84,13 @@ void output_model_grid (geometry_struct& geometry,
     NumUtils::Cube<float> tmp_tau;
     tmp_tau.CSize(geometry.grids[m].index_dim[0],geometry.grids[m].index_dim[1],geometry.grids[m].index_dim[2]);
 
+		// create a 3d matrix to copy the grid info into for output
+    NumUtils::Cube<double> tmp_num_H;
+    tmp_num_H.CSize(geometry.grids[m].index_dim[0],geometry.grids[m].index_dim[1],geometry.grids[m].index_dim[2]);
+
     // get the max index size
     int max_index = geometry.grids[m].index_dim[0];
-    for (i = 1; i < 3; i++) 
+    for (i = 1; i < 3; i++)
       if (geometry.grids[m].index_dim[i] > max_index) max_index = geometry.grids[m].index_dim[i];
 
     // create a 3d matrix to copy the grid info into for output
@@ -99,27 +109,28 @@ void output_model_grid (geometry_struct& geometry,
       tmp_pos(i,1) = geometry.grids[m].positions[1][i];
     for (i = 0; i <= geometry.grids[m].index_dim[2]; i++)
       tmp_pos(i,2) = geometry.grids[m].positions[2][i];
-    
+
     // loop of the cells in this grid
     float rad_unc = 0.0;
     for (k = 0; k < geometry.grids[m].index_dim[2]; k++)
       for (j = 0; j < geometry.grids[m].index_dim[1]; j++)
-	for (i = 0; i < geometry.grids[m].index_dim[0]; i++) {
-	  tmp_tau(i,j,k) = geometry.grids[m].grid(i,j,k).dust_tau_per_pc;
-	  for (n = 0; n < n_waves; n++) {
-	    tmp_rad_field(i,j,k,n) = geometry.grids[m].grid(i,j,k).absorbed_energy[n];
-	    if (geometry.grids[m].grid(i,j,k).absorbed_energy_num_photons[n] >= 5) {
-	      rad_unc = geometry.grids[m].grid(i,j,k).absorbed_energy_x2[n]/geometry.grids[m].grid(i,j,k).absorbed_energy_num_photons[n] -
-		pow(double(geometry.grids[m].grid(i,j,k).absorbed_energy[n]/geometry.grids[m].grid(i,j,k).absorbed_energy_num_photons[n]),double(2.0));
-	      if (rad_unc > 0.0)
-		rad_unc = sqrt(rad_unc);
-	      else
-		rad_unc = 0.0;
-	      tmp_rad_field_unc(i,j,k,n) = rad_unc;
-	    }
-	  }
-	}
-  
+				for (i = 0; i < geometry.grids[m].index_dim[0]; i++) {
+	  			tmp_tau(i,j,k) = geometry.grids[m].grid(i,j,k).dust_tau_per_pc;
+					tmp_num_H(i,j,k) = geometry.grids[m].grid(i,j,k).num_H;
+	  			for (n = 0; n < n_waves; n++) {
+	    			tmp_rad_field(i,j,k,n) = geometry.grids[m].grid(i,j,k).absorbed_energy[n];
+	    			if (geometry.grids[m].grid(i,j,k).absorbed_energy_num_photons[n] >= 5) {
+	      			rad_unc = geometry.grids[m].grid(i,j,k).absorbed_energy_x2[n]/geometry.grids[m].grid(i,j,k).absorbed_energy_num_photons[n] -
+								pow(double(geometry.grids[m].grid(i,j,k).absorbed_energy[n]/geometry.grids[m].grid(i,j,k).absorbed_energy_num_photons[n]),double(2.0));
+	      			if (rad_unc > 0.0)
+								rad_unc = sqrt(rad_unc);
+	      			else
+								rad_unc = 0.0;
+	      			tmp_rad_field_unc(i,j,k,n) = rad_unc;
+	    			}
+	  			}
+				}
+
 #ifdef DEBUG_OMG
     cout << "done filling the temp vectors" << endl;
 #endif
@@ -135,8 +146,8 @@ void output_model_grid (geometry_struct& geometry,
 
     fits_create_img(out_ptr, -32, 4, four_vector_size, &status);
     check_fits_io(status,"fits_create_image : output_model_grid (rad_field)");
-      
-    fits_write_img(out_ptr, TFLOAT, 1, geometry.grids[m].index_dim[0]*geometry.grids[m].index_dim[1]*geometry.grids[m].index_dim[2]*n_waves, 
+
+    fits_write_img(out_ptr, TFLOAT, 1, geometry.grids[m].index_dim[0]*geometry.grids[m].index_dim[1]*geometry.grids[m].index_dim[2]*n_waves,
 		   &tmp_rad_field[0], &status);
 
 #ifdef DEBUG_OMG
@@ -146,8 +157,8 @@ void output_model_grid (geometry_struct& geometry,
     // create and output each grid (rad_field unc)
     fits_create_img(out_unc_ptr, -32, 4, four_vector_size, &status);
     check_fits_io(status,"fits_create_image : output_model_grid (rad_field unc)");
-      
-    fits_write_img(out_unc_ptr, TFLOAT, 1, geometry.grids[m].index_dim[0]*geometry.grids[m].index_dim[1]*geometry.grids[m].index_dim[2]*n_waves, 
+
+    fits_write_img(out_unc_ptr, TFLOAT, 1, geometry.grids[m].index_dim[0]*geometry.grids[m].index_dim[1]*geometry.grids[m].index_dim[2]*n_waves,
 		   &tmp_rad_field_unc[0], &status);
 
 #ifdef DEBUG_OMG
@@ -157,9 +168,16 @@ void output_model_grid (geometry_struct& geometry,
     // create and output each grid (tau)
     fits_create_img(out_tau_ptr, -32, 3, geometry.grids[m].index_dim, &status);
     check_fits_io(status,"fits_create_image : output_model_grid (tau)");
-      
-    fits_write_img(out_tau_ptr, TFLOAT, 1, geometry.grids[m].index_dim[0]*geometry.grids[m].index_dim[1]*geometry.grids[m].index_dim[2], 
+
+    fits_write_img(out_tau_ptr, TFLOAT, 1, geometry.grids[m].index_dim[0]*geometry.grids[m].index_dim[1]*geometry.grids[m].index_dim[2],
 		   &tmp_tau[0], &status);
+
+		// create and output each grid (num_H)
+    fits_create_img(out_num_H_ptr, -64, 3, geometry.grids[m].index_dim, &status);
+    check_fits_io(status,"fits_create_image : output_model_grid (num_H)");
+
+    fits_write_img(out_num_H_ptr, TDOUBLE, 1, geometry.grids[m].index_dim[0]*geometry.grids[m].index_dim[1]*geometry.grids[m].index_dim[2],
+		   &tmp_num_H[0], &status);
 
     // create and output each grid (positions)
     long tmp_pos_index[2];
@@ -167,8 +185,8 @@ void output_model_grid (geometry_struct& geometry,
     tmp_pos_index[1] = 3;
     fits_create_img(out_pos_ptr, -64, 2, tmp_pos_index, &status);
     check_fits_io(status,"fits_create_image : output_model_grid (pos)");
-      
-    fits_write_img(out_pos_ptr, TDOUBLE, 1, tmp_pos_index[0]*tmp_pos_index[1], 
+
+    fits_write_img(out_pos_ptr, TDOUBLE, 1, tmp_pos_index[0]*tmp_pos_index[1],
 		   &tmp_pos[0], &status);
 
 #ifdef DEBUG_OMG
@@ -185,7 +203,7 @@ void output_model_grid (geometry_struct& geometry,
       fits_write_key(out_tau_ptr, TINT, "PAR_GRID", &geometry.grids[m].parent_grid_num, "grid number of partent", &status);
     } else {
       // populate the primary header with the details of the run
-      
+
       // final stuff for primary header
       fits_write_comment(out_ptr, "**---------------------------------**",&status);
       fits_write_comment(out_ptr, "Output of the DIRTY model",&status);
@@ -201,7 +219,7 @@ void output_model_grid (geometry_struct& geometry,
       fits_write_comment(out_unc_ptr, "version v2.0prealpha (Oct 2009)", &status);
       fits_write_comment(out_unc_ptr, "**---------------------------------**",&status);
       check_fits_io(status,"fits_write_comment : output_model_grid (unc)");
-      
+
       // extra information needed for setting up the geometry
       fits_write_key(out_tau_ptr, TFLOAT, "RAD_TAU", &geometry.tau, "radial optical depth", &status);
       fits_write_key(out_tau_ptr, TLONG, "GRDDEPTH", &geometry.max_grid_depth, "maximum depth of the grid", &status);
@@ -213,7 +231,15 @@ void output_model_grid (geometry_struct& geometry,
       fits_write_comment(out_tau_ptr, "version v2.0prealpha (Oct 2009)", &status);
       fits_write_comment(out_tau_ptr, "**---------------------------------**",&status);
       check_fits_io(status,"fits_write_comment : output_model_grid (tau)");
-      
+
+			// final stuff for primary header
+      fits_write_comment(out_num_H_ptr, "**---------------------------------**",&status);
+      fits_write_comment(out_num_H_ptr, "Output of the DIRTY model",&status);
+      fits_write_comment(out_num_H_ptr, "Karl D. Gordon & Karl A. Misselt", &status);
+      fits_write_comment(out_num_H_ptr, "version v2.0prealpha (Oct 2009)", &status);
+      fits_write_comment(out_num_H_ptr, "**---------------------------------**",&status);
+      check_fits_io(status,"fits_write_comment : output_model_grid (num_H)");
+
       // final stuff for primary header
       fits_write_comment(out_pos_ptr, "**---------------------------------**",&status);
       fits_write_comment(out_pos_ptr, "Output of the DIRTY model",&status);
@@ -230,7 +256,7 @@ void output_model_grid (geometry_struct& geometry,
   wavelength_size[0] = n_waves;
   fits_create_img(out_wave_ptr, -32, 1, wavelength_size, &status);
   check_fits_io(status,"fits_create_image : output_model_grid (wavelength grid)");
-  
+
   fits_write_img(out_wave_ptr, TFLOAT, 1, n_waves, &runinfo.wavelength[0], &status);
 
   // close FITS File
@@ -248,6 +274,10 @@ void output_model_grid (geometry_struct& geometry,
   // close FITS File
   fits_close_file(out_tau_ptr, &status);
   check_fits_io(status,"fits_close_file : output_model_grid (tau)");
+
+	// close FITS File
+  fits_close_file(out_num_H_ptr, &status);
+  check_fits_io(status,"fits_close_file : output_model_grid (num_H)");
 
   // close FITS File
   fits_close_file(out_pos_ptr, &status);
