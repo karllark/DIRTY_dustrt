91e8e4d933716a67ccb90d00d6fef3f997986315
Karl D. Gordon <kgordon@stsci.edu>
2023-11-21 09:28:13 -0500

    fixing theme

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'DIRTY'
-copyright = '2018, Karl Gordon'
+copyright = '2023, Karl Gordon'
 author = 'Karl Gordon'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -83,8 +83,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
+html_theme = 'alabaster'
+# html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
