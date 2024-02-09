e7ee6ad9413a7b36febda20e5b6960a16f1bcb1a
Karl D. Gordon <kgordon@stsci.edu>
2024-02-09 13:26:35 -0500

    adding in code to compute and write files for DGFit

---
+++ write_grain/include/ConfigFile.h
@@ -0,0 +1,66 @@
+/**************************************************************
+ * ConfigFile -- ConfigFile Class, header file.            
+ * 
+ * History: 
+ *     Written by:   Putrid, August 2004
+ *                   Contact: misselt@as.arizona.edu
+ *
+ *
+ **************************************************************/
+#ifndef __CONFIG_FILE_H__
+#define __CONFIG_FILE_H__
+
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+class ConfigFile {
+
+ private: 
+  map<string,string> content_;
+  string _BadString;
+  double _BadDouble; 
+  float _BadFloat;
+  int _BadInt; 
+  long _BadLong;
+  
+ public:
+  ConfigFile(string const& configFile);
+  
+  bool BValue(string const& section, string const& entry) const;
+  string SValue(string const& section, string const& entry) const;
+  double DValue(string const& section, string const& entry) const; 
+  float  FValue(string const& section, string const& entry) const; 
+  int    IValue(string const& section, string const& entry) const;
+  long   LValue(string const& section, string const& entry) const;
+
+  inline string BadString ( void ) { return _BadString; } 
+  inline bool BadFloat ( float _val ) { return _val != _val; } 
+  inline bool BadDouble ( double _val ) { return _val != _val; } 
+  inline int BadInt ( void ) { return _BadInt; } 
+  inline long BadLong ( void ) { return _BadLong; }
+
+  inline bool isBadFloat ( float _val ) { 
+    if (isnan(_val)) 
+      return true; 
+    else 
+      return false; 
+  }
+
+  inline bool isBadDouble( float _val ) { 
+    if (isnan(_val)) 
+      return true; 
+    else 
+      return false; 
+  }
+ 
+
+};
+
+#endif
---
+++ write_grain/include/check_input_param.h
@@ -0,0 +1,36 @@
+#ifndef _DIRTY_CHECK_INPUT_PARAM_
+#define _DIRTY_CHECK_INPUT_PARAM_
+
+//**********************************************************************
+// template function definitions
+
+// check for required numerical parameter with min/max bounds
+template <class FI, class FI2, class FI3>
+    void check_input_param(string const& param, FI x, FI2 min_x, FI3 max_x)
+{
+  // first check if the value is set
+  if ((x == -99) || (!finite(x))) {
+    cout << "Required parameter " << param << " not set." << endl;
+    exit(8);
+  } else if ((x < min_x) || (x > max_x)) { // then check it is between the limits
+    cout << "Parameter '" << param << "' (= " << x << ") is outside of allowed range = [";
+    cout << min_x << "," << max_x << "]" << endl;
+    exit(8);
+  }
+}
+
+// set required string parameter to default value
+template <class FI>
+    void check_input_param(string const& param, FI x, string const& def_x)
+{
+  // if not set, then set to default and print warning
+  if (x ==  "NULL") {
+    cout << "Setting required parameter '" << param << "' to default value of '";
+    cout << def_x << "'" << endl;
+  }
+}
+
+//**********************************************************************
+// external function definitions
+
+#endif
